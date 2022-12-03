e50fea191cc7e9811a6aaaa76cc99d70b30e3421
bhorvitz <brian.horvitz@gmail.com>
2022-12-03 08:06:00 -0800

    Update payment.py
    Add spam filter of 1000 mojos to notifications.  This would probably be better as a config option.

--- monitor/notifications/payment.py
+++ monitor/notifications/payment.py
@@ -20,6 +20,7 @@ class PaymentNotification(Notification):
     def trigger(self) -> None:
         with session() as db_session:
             last_payment_mojos = get_last_payment(db_session)
-        return self.apobj.notify(title='** 🤑 Payment received! 🤑 **',
-                                 body="Your wallet received a new payment\n" + \
-                                     f"🌱 +{last_payment_mojos/1e12:.5f} XCH")
+        if last_payment_mojos > 1000:
+            return self.apobj.notify(title='** 🤑 Payment received! 🤑 **',
+                                     body="Your wallet received a new payment\n" + \
+                                         f"🌱 +{last_payment_mojos/1e12:.5f} XCH")
